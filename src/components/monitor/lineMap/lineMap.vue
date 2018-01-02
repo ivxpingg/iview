a07c167520a67495a14eeaa586560955b42089b3
sgj <764341699@qq.com>
2018-01-02 23:13:12 +0800

    lineMap.vue 更新

--- src/components/monitor/lineMap/lineMap.vue
+++ src/components/monitor/lineMap/lineMap.vue
@@ -5,6 +5,202 @@
 
         <div class="station station-24">岩内</div>
         <div class="station station-1">镇海路</div>
+        <div class="down-station-box sxc-flex">
+            <div class="station sxc-flex__item">
+                <div class="name">岩内</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">厦门北站</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">天水路</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">集美大道</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">集美软件园</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">诚毅广场</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">官任</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">杏锦路</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">杏林村</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">园博苑</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">集美学村</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">高崎</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">殿前</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">火炬园</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">塘边</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">乌石浦</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">吕厝</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">莲花路口</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">莲坂</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">湖滨东路</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">文灶</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">将军祠</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">中山公园</div>
+                <div class="circle"></div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="name">镇海路</div>
+                <div class="circle"></div>
+            </div>
+        </div>
+        <div class="up-station-box sxc-flex">
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">岩内</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">厦门北站</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">天水路</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">集美大道</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">集美软件园</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">诚毅广场</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">官任</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">杏锦路</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">杏林村</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">园博苑</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">集美学村</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">高崎</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">殿前</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">火炬园</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">塘边</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">乌石浦</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">吕厝</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">莲花路口</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">莲坂</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">湖滨东路</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">文灶</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">将军祠</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">中山公园</div>
+            </div>
+            <div class="station sxc-flex__item">
+                <div class="circle"></div>
+                <div class="name">镇海路</div>
+            </div>
+        </div>
 
     </div>
 </template>
@@ -17,4 +213,66 @@
     }
 </script>
 
-<style lang="scss" rel="stylesheet/scss" scoped></style>
+<style lang="scss" rel="stylesheet/scss" scoped>
+    .container {
+        .up-station-box{
+            background-color: #bac6ff;
+
+            height: 200px;
+            border: 1px solid #666;
+
+            .station {
+                margin-top: 50px;
+                position: relative;
+                overflow: hidden;
+                .circle {
+                    margin: 0 auto;
+                    width: 16px;
+                    height:16px;
+                    border: 2px solid #3c3c3c;
+                    border-radius: 50%;
+                }
+                .name {
+                    margin: 0 auto;
+                    font-size: 12px;
+                    width: 16px;
+                    line-height: 16px;
+                    text-align: center;
+                }
+            }
+        }
+
+        .down-station-box{
+            position: relative;
+            background-color: #bac6ff;
+            height: 200px;
+            border: 1px solid #666;
+
+            .station {
+                position: relative;
+                bottom: 50px;
+                overflow: hidden;
+                .circle {
+                    position: absolute;
+                    bottom: 0;
+                    left: 50%;
+                    margin-left: -8px;
+                    width: 16px;
+                    height:16px;
+                    border: 2px solid #3c3c3c;
+                    border-radius: 50%;
+                }
+                .name {
+                    position: absolute;
+                    bottom: 16px;
+                    left: 50%;
+                    margin-left: -8px;
+                    font-size: 12px;
+                    width: 16px;
+                    line-height: 16px;
+                    text-align: center;
+                }
+            }
+        }
+    }
+</style>
