5be18e8f3ad59a1dca08a08433f3ae9605517d42
ivxpingg <764341699@qq.com>
2018-03-08 17:32:43 +0800

    1.对接视频数后台接口

--- src/components/monitor/routerView/videoMonitor.vue
+++ src/components/monitor/routerView/videoMonitor.vue
@@ -34,114 +34,115 @@
             return {
                 searchValue: '',     // 检索表单控件model
                 keyword: '',         // 检索关键字
-                data2: [
-                    {
-                        title: '视频监控',
-                        expand: true,
-                        render(h, {root, node, data}) {
-                            return that.renderContent(h, {root, node, data});
-                        },
-                        children: [
-                            {
-                                title: '厦门市公安局',
-                                expand: true,
-                                render(h, {root, node, data}) {
-                                    return that.renderContent(h, {root, node, data});
-                                },
-                                children: [
-                                    {
-                                        title: '厦禾路',
-                                        expand: true,
-                                        render(h, {root, node, data}) {
-                                            return that.renderContent(h, {root, node, data});
-                                        },
-                                        children: [
-                                            {
-                                                title: '2028厦禾路白鹭洲路口',
-                                                puid: 'c34fdcecdef6434db242dbc2fdbee61d@kedacom',
-                                                render(h, {root, node, data}) {
-                                                   return that.renderContent(h, {root, node, data});
-                                                }
-                                            },
-                                            {
-                                                title: '厦禾路银行中心路口3',
-                                                puid: '802e4068b3904192948fbbb7f9c5f511@kedacom',
-                                                render(h, {root, node, data}) {
-                                                    return that.renderContent(h, {root, node, data});
-                                                }
-                                            },
-                                            {
-                                                title: '厦禾路公园小学路口3bf',
-                                                puid: '7216441fdd4043bc9cc13d8fcb780aee@kedacom',
-                                                render(h, {root, node, data}) {
-                                                    return that.renderContent(h, {root, node, data});
-                                                }
-                                            }
-                                        ]
-                                    }
-                                ]
-                            },
-                            {
-                                title: '厦门BRT',
-                                expand: true,
-                                render(h, {root, node, data}) {
-                                    return that.renderContent(h, {root, node, data});
-                                },
-                                children: [
-                                    {
-                                        title: '双十中学站',
-                                        expand: true,
-                                        render(h, {root, node, data}) {
-                                            return that.renderContent(h, {root, node, data});
-                                        },
-                                        children: [
-                                            {
-                                                title: '双十中学站_上行电梯',
-                                                puid: '86ccb50b45b44e9b9be229b9e1a25b7e@kedacom',
-                                                render(h, {root, node, data}) {
-                                                    return that.renderContent(h, {root, node, data});
-                                                }
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        title: '洪文站',
-                                        expand: true,
-                                        render(h, {root, node, data}) {
-                                            return that.renderContent(h, {root, node, data});
-                                        },
-                                        children: [
-                                            {
-                                                title: '洪文站_上行电梯',
-                                                puid: '8e3a0705cfa440f6b1c0310e2d47ecc0@kedacom',
-                                                render(h, {root, node, data}) {
-                                                    return that.renderContent(h, {root, node, data});
-                                                }
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        title: '集美大桥南站',
-                                        expand: true,
-                                        render(h, {root, node, data}) {
-                                            return that.renderContent(h, {root, node, data});
-                                        },
-                                        children: [
-                                            {
-                                                title: '集南站_上行站厅2',
-                                                puid: 'fae2e4ae53ef4263bf9d2be56743285a@kedacom',
-                                                render(h, {root, node, data}) {
-                                                    return that.renderContent(h, {root, node, data});
-                                                }
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ],
-
-                    }
+                ajaxData: [
+//                    {
+//                        title: '视频监控',
+//                        expand: true,
+//                        render(h, {root, node, data}) {
+//                            return that.renderContent(h, {root, node, data});
+//                        },
+//                        children: [
+//                            {
+//                                title: '厦门市公安局',
+//                                expand: true,
+//                                render(h, {root, node, data}) {
+//                                    return that.renderContent(h, {root, node, data});
+//                                },
+//                                children: [
+//                                    {
+//                                        title: '厦禾路',
+//                                        expand: true,
+//                                        render(h, {root, node, data}) {
+//                                            return that.renderContent(h, {root, node, data});
+//                                        },
+//                                        children: [
+//                                            {
+//                                                title: '2028厦禾路白鹭洲路口',
+//                                                puid: 'c34fdcecdef6434db242dbc2fdbee61d@kedacom',
+//                                                render(h, {root, node, data}) {
+//                                                   return that.renderContent(h, {root, node, data});
+//                                                }
+//                                            },
+//                                            {
+//                                                title: '厦禾路银行中心路口3',
+//                                                puid: '802e4068b3904192948fbbb7f9c5f511@kedacom',
+//                                                render(h, {root, node, data}) {
+//                                                    return that.renderContent(h, {root, node, data});
+//                                                }
+//                                            },
+//                                            {
+//                                                title: '厦禾路公园小学路口3bf',
+//                                                puid: '7216441fdd4043bc9cc13d8fcb780aee@kedacom',
+//                                                render(h, {root, node, data}) {
+//                                                    return that.renderContent(h, {root, node, data});
+//                                                }
+//                                            }
+//                                        ]
+//                                    }
+//                                ]
+//                            },
+//                            {
+//                                title: '厦门BRT',
+//                                expand: true,
+//                                render(h, {root, node, data}) {
+//                                    return that.renderContent(h, {root, node, data});
+//                                },
+//                                children: [
+//                                    {
+//                                        title: '双十中学站',
+//                                        expand: true,
+//                                        render(h, {root, node, data}) {
+//                                            return that.renderContent(h, {root, node, data});
+//                                        },
+//                                        children: [
+//                                            {
+//                                                title: '双十中学站_上行电梯',
+//                                                puid: '86ccb50b45b44e9b9be229b9e1a25b7e@kedacom',
+//                                                render(h, {root, node, data}) {
+//                                                    return that.renderContent(h, {root, node, data});
+//                                                }
+//                                            }
+//                                        ]
+//                                    },
+//                                    {
+//                                        title: '洪文站',
+//                                        expand: true,
+//                                        render(h, {root, node, data}) {
+//                                            return that.renderContent(h, {root, node, data});
+//                                        },
+//                                        children: [
+//                                            {
+//                                                title: '洪文站_上行电梯',
+//                                                puid: '8e3a0705cfa440f6b1c0310e2d47ecc0@kedacom',
+//                                                render(h, {root, node, data}) {
+//                                                    return that.renderContent(h, {root, node, data});
+//                                                }
+//                                            }
+//                                        ]
+//                                    },
+//                                    {
+//                                        title: '集美大桥南站',
+//                                        expand: true,
+//                                        render(h, {root, node, data}) {
+//                                            return that.renderContent(h, {root, node, data});
+//                                        },
+//                                        children: [
+//                                            {
+//                                                title: '集南站_上行站厅2',
+//                                                puid: 'fae2e4ae53ef4263bf9d2be56743285a@kedacom',
+//                                                render(h, {root, node, data}) {
+//                                                    return that.renderContent(h, {root, node, data});
+//                                                }
+//                                            }
+//                                        ]
+//                                    }
+//                                ]
+//                            }
+//                        ],
+//
+//                    }
                 ],
+                treeData: [],
 
                 searchData: [],
 
@@ -154,7 +155,7 @@
             keyword(value) {
                 var that = this;
                 var datas = [];
-                this.data2.forEach(function (obj) {
+                this.treeData.forEach(function (obj) {
                     var o = {
                         title: obj.title,
                         expand: true,
@@ -169,15 +170,31 @@
                 });
 
                 this.searchData = datas;
+            },
+            ajaxData(val) {
+                var that = this;
+                var obj = {
+                    title: '视频监控',
+                    expand: true,
+                    render(h, {root, node, data}) {
+                        return that.renderContent(h, {root, node, data});
+                    },
+                    children: []
+                };
+
+                val.forEach(function (v) {
+                   obj.children.push(that.callBackDataToTreeData(v));
+                });
+
+                that.treeData = [obj];
+                that.searchData = that.treeData;
             }
         },
-        created() {
-            this.searchData = this.data2;
-        },
+        created() { },
         mounted() {
             var that = this;
 
-            // this.onMessage();
+            this.getData();
 
             this.contentWindow = document.getElementById("iframe_video").contentWindow;
 
@@ -198,6 +215,52 @@
             },
             initIframeSrc() {
                 this.iframeSrc = "/html/video.html";
+            },
+
+            // ajax 获取树
+            getData() {
+                var that = this;
+                this.$Spin.show();
+
+                Util.ajax({
+                    method: "get",
+                    url: '/xm/run/videoPosition/getVideoPositionInfo',
+                    data: {}
+                }).then(function(response){
+                    that.$Spin.hide();
+                    if (response.status === 1) {
+                        that.ajaxData = response.result.videoPositionList;
+                    }
+                    else {}
+                }).catch(function (error) {
+                    that.$Spin.hide();
+                    console.log(error);
+                })
+            },
+
+            callBackDataToTreeData(children) {
+                var that = this;
+
+                var obj = {
+                    title: children.groupName,
+                    expand: false,
+                    render(h, {root, node, data}) {
+                        return that.renderContent(h, {root, node, data});
+                    },
+                    children: []
+                }
+
+                if (children.puId) {
+                    obj.puid = children.puId;
+                }
+
+                if (children.childVideoPosition && children.childVideoPosition.length > 0) {
+                    children.childVideoPosition.forEach(function (v) {
+                        obj.children.push(that.callBackDataToTreeData(v));
+                    })
+                }
+
+                return obj;
             },
 
             //  提交检索
